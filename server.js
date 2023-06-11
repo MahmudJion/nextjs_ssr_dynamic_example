66fea4d7d94e1d9ad8099698daf9bc8671baa0c9
Mahmud Hasan Jion <mahmudh.jion02@gmail.com>
2023-06-11 20:04:12 +0100

    update server.js

--- server.js
+++ server.js
@@ -1,27 +1,25 @@
-const { createServer } = require('http')
-const { parse } = require('url')
-const next = require('next')
+import { createServer } from 'http';
+import { parse } from 'url';
+import next from 'next';
 
-const prod = process.env.NODE_ENV == 'production'
-const app = next({ prod })
-const handle = app.getRequestHandler()
+const prod = process.env.NODE_ENV === 'production';
+const app = next({ dev: !prod });
+const handle = app.getRequestHandler();
 
 app.prepare().then(() => {
   createServer((req, res) => {
-    // Be sure to pass `true` as the second argument to `url.parse`.
-    // This tells it to parse the query portion of the URL.
-    const parsedUrl = parse(req.url, true)
-    const { pathname, query } = parsedUrl
+    const parsedUrl = parse(req.url, true);
+    const { pathname, query } = parsedUrl;
 
     if (pathname === '/a') {
-      app.render(req, res, '/a', query)
+      app.render(req, res, '/a', query);
     } else if (pathname === '/b') {
-      app.render(req, res, '/b', query)
+      app.render(req, res, '/b', query);
     } else {
-      handle(req, res, parsedUrl)
+      handle(req, res, parsedUrl);
     }
   }).listen(3000, (err) => {
-    if (err) throw err
-    console.log('> Ready on http://localhost:3000')
-  })
-})
+    if (err) throw err;
+    console.log('> Ready on http://localhost:3000');
+  });
+});
