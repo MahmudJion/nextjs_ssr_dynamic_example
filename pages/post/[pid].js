1fbd2cdb9dbdefd267e7fdd4f69cc541146de922
Mahmud Hasan Jion <mahmudh.jion02@gmail.com>
2023-06-11 20:03:06 +0100

    update [pid].js

--- pages/post/[pid].js
+++ pages/post/[pid].js
@@ -1,35 +1,52 @@
 import axios from 'axios';
-import Head from 'next/head'
-import styles from '../../styles/Home.module.css'
-import Link from 'next/link'
+import { useEffect } from 'react';
+import { useRouter } from 'next/router';
+import Head from 'next/head';
+import Link from 'next/link';
+import styles from '../../styles/Home.module.css';
 
-function Post ({ post }) {
-    return (
-        <div className={styles.container}>
-            <Head>
-                <title>{post.title}</title>
-                <link rel="icon" href="/favicon.ico" />
-            </Head>
+function Post({ post }) {
+  const router = useRouter();
 
-            <main className={styles.main}>
-                <div className={styles.card}>
-                    <h3>{post.title}</h3>
-                    <p>{post.body}</p>
-                </div>
-                <Link href="/">
-                    <button>Home</button>
-                </Link>
-            </main>
-        </div>
-    );
-}
-
-Post.getInitialProps = async (ctx) => {
-    const response = await axios.get(`https://jsonplaceholder.typicode.com/posts/${ctx.query.pid}`,
-    )
-    return {
-        post: response.data
+  useEffect(() => {
+    // Redirect to home page if post data is not available
+    if (!post) {
+      router.push('/');
     }
+  }, [post, router]);
+
+  if (!post) {
+    return null; // or show a loading state
+  }
+
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>{post.title}</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main className={styles.main}>
+        <div className={styles.card}>
+          <h3>{post.title}</h3>
+          <p>{post.body}</p>
+        </div>
+        <button onClick={() => router.back()}>Home</button>
+      </main>
+    </div>
+  );
 }
 
-export default Post
+export async function getServerSideProps(ctx) {
+  const response = await axios.get(
+    `https://jsonplaceholder.typicode.com/posts/${ctx.query.pid}`
+  );
+
+  return {
+    props: {
+      post: response.data,
+    },
+  };
+}
+
+export default Post;
