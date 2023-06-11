d5b9596b80b0f77dab8b4bc4754c13870b39cde5
Mahmud Hasan Jion <mahmudh.jion02@gmail.com>
2023-06-11 20:01:20 +0100

    update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,58 +1,59 @@
 import axios from 'axios';
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
-import Link from 'next/link'
+import Head from 'next/head';
+import styles from '../styles/Home.module.css';
+import Link from 'next/link';
 
-const Home = (props) => (
+export default function Home({ posts }) {
+  return (
     <div className={styles.container}>
-        <Head>
-            <title>Nextjs Blog</title>
-            <link rel="icon" href="/favicon.ico" />
-        </Head>
+      <Head>
+        <title>Next.js Blog</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
 
-        <main className={styles.main}>
-            <h1 className={styles.title}>
-                Welcome to <a href="https://nextjs.org">Next.js Blog</a>
-            </h1>
+      <main className={styles.main}>
+        <h1 className={styles.title}>
+          Welcome to <a href="https://nextjs.org">Next.js Blog</a>
+        </h1>
 
-            <div className={styles.grid}>
-                {props.posts.map((post, index) => {
-                    return (
-                        <Link
-                            as={"/post/" + post.id}
-                            href="/post/[pid]"
-                            key={index}
-                        >
-                            <a className={styles.card}>
-                                <h3>{post.title}</h3>
-                                <p>{post.body}</p>
-                            </a>
-                        </Link>
-                    )
-                })}
-            </div>
-        </main>
-
-        <footer className={styles.footer}>
-            <a
-                href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-                target="_blank"
-                rel="noopener noreferrer"
+        <div className={styles.grid}>
+          {posts.map((post) => (
+            <Link
+              key={post.id}
+              as={`/post/${post.id}`}
+              href="/post/[pid]"
             >
-                Powered by{' '}
-                <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-            </a>
-        </footer>
+              <a className={styles.card}>
+                <h3>{post.title}</h3>
+                <p>{post.body}</p>
+              </a>
+            </Link>
+          ))}
+        </div>
+      </main>
+
+      <footer className={styles.footer}>
+        <a
+          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Powered by{' '}
+          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
+        </a>
+      </footer>
     </div>
-);
-
-Home.getInitialProps = async function () {
-
-    const response = await axios.get('https://jsonplaceholder.typicode.com/posts?_start=0&_limit=10',
-    )
-    return {
-        posts: response.data
-    }
+  );
 }
 
-export default Home;
+export async function getStaticProps() {
+  const response = await axios.get(
+    'https://jsonplaceholder.typicode.com/posts?_start=0&_limit=10'
+  );
+
+  return {
+    props: {
+      posts: response.data,
+    },
+  };
+}
